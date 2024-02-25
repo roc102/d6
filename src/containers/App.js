e95989e5b58494bfc4107a2d71b28a24a62077fe
roc102 <mdfahadrahman1969@gmail.com>
2024-02-25 17:25:18 +0530

    init

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "tachyons";
+
+const App = () => {
+  return (
+    <div className="tc">
+      <h1 className="f2">RoboFriends</h1>
+    </div>
+  );
+};
+
+export default App;
