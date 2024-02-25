aa26ef3caeb18daf72aad5dd9996e2648019b715
roc102 <mdfahadrahman1969@gmail.com>
2024-02-25 17:26:40 +0530

    d1

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import "tachyons";
+import Searchbox from "../components/Searchbox";
 
 const App = () => {
   return (
     <div className="tc">
       <h1 className="f2">RoboFriends</h1>
+      <Searchbox />
     </div>
   );
 };
