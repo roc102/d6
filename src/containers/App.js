e723750dca209ea1f316ec75288fe9d1c951d9e0
roc102 <mdfahadrahman1969@gmail.com>
2024-02-25 17:31:43 +0530

    d2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "tachyons";
 import Searchbox from "../components/Searchbox";
-import Card from "../components/Card";
+import CardList from "../components/CardList";
 
 const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((robots) => setRobots(robots));
+  }, []);
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const filterRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
   return (
     <div className="tc">
       <h1 className="f2">RoboFriends</h1>
-      <Searchbox />
-      <Card />
+      <Searchbox SearchChange={onSearchChange} />
+      <CardList robots={filterRobots} />
     </div>
   );
 };
